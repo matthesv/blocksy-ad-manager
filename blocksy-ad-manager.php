95a268992b50995ae8b4f802e34c34a12e216fd1
Matthes Vogel <matthesv@gmail.com>
2025-12-23 21:14:17 +0100

    Init

---
+++ blocksy-ad-manager.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Plugin Name: Blocksy Ad Manager
+ * Description: Flexibles Anzeigen-Management für Blocksy Theme
+ * Version: 1.0.0
+ * Author: Matthes Vogel
+ * Text Domain: blocksy-ad-manager
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+define('BAM_VERSION', '1.0.0');
+define('BAM_PLUGIN_DIR', plugin_dir_path(__FILE__));
+define('BAM_PLUGIN_URL', plugin_dir_url(__FILE__));
+
+// Autoloader
+spl_autoload_register(function ($class) {
+    $prefix = 'BAM_';
+    if (strpos($class, $prefix) !== 0) {
+        return;
+    }
+    
+    $class_name = str_replace($prefix, '', $class);
+    $class_name = strtolower(str_replace('_', '-', $class_name));
+    $file = BAM_PLUGIN_DIR . 'includes/class-ad-manager-' . $class_name . '.php';
+    
+    if (file_exists($file)) {
+        require_once $file;
+    }
+});
+
+// Plugin initialisieren
+function bam_init() {
+    $loader = new BAM_Loader();
+    $loader->run();
+}
+add_action('plugins_loaded', 'bam_init');
+
+// Aktivierung
+register_activation_hook(__FILE__, function() {
+    flush_rewrite_rules();
+});
